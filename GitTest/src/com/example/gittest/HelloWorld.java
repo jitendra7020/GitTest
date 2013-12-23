5c0b6ec8a19ae034ddb9c472b7b4d97b4f0c05f4
Jitendra Nath <jitendra7020@gmail.com>
2013-12-23 19:32:36 +0530

    Hello world added

---
+++ GitTest/src/com/example/gittest/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.gittest;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Hello world class");
+	}
+
+}
