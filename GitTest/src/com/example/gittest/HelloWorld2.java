f5fc52ae0de0c723fff89606c1a3305aab191674
Jitendra Nath <jitendra7020@gmail.com>
2013-12-23 19:52:25 +0530

    Hellowworld3 added

---
+++ GitTest/src/com/example/gittest/HelloWorld2.java
@@ -0,0 +1,13 @@
+package com.example.gittest;
+
+public class HelloWorld2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Hello world2 class");
+	}
+
+}
