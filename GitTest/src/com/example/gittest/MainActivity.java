646d9903555d8d34680bbd29151c230601eff4b8
Jitendra Nath <jitendra7020@gmail.com>
2013-12-23 19:23:01 +0530

    Second commit

--- GitTest/src/com/example/gittest/MainActivity.java
+++ GitTest/src/com/example/gittest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 System.out.println("Git is cool");
+		 System.out.println("Now i'm updating the log.");
 	}
 
 	@Override
---
+++ GitTest/src/com/example/gittest/TestClass.java
@@ -0,0 +1,15 @@
+package com.example.gittest;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is test class");
+
+	}
+
+}
